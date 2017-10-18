4a673c0a5699ffdb2c2243c11adb5b72b51aed18
Leon <raioneru@utexas.edu>
2017-10-18 01:17:00 -0500

    Added printResults and PrintQuestions method to have less redundant code.

--- Phonetic.java
+++ Phonetic.java
@@ -16,6 +16,7 @@ class Phonetic{
 	while(inputCont.equalsIgnoreCase("y")){
 		//initalize points each round
 		int points = 0;
+		int intNumQuestions = 26;
 		//Create index so no duplicate questions are asked
 		Integer[] index = new Integer[alphabetArray.length];
 		for (int i = 0; i < index.length; i++) { 
@@ -24,62 +25,59 @@ class Phonetic{
 		//Randomize the array
 		Collections.shuffle(Arrays.asList(index));
 		//Ask user how many rounds they want to do
-		System.out.println("How many questions?: (Type ALL if you want to get full quiz");
+		System.out.println("How many questions?: (Type ALL if you want to get full quiz)");
 		Scanner scannerNumQuestions = new Scanner(System.in);
 		String inputNumQuestions = scannerNumQuestions.next();
 		//Branch based on users response
 		if(inputNumQuestions.equalsIgnoreCase("all")){
-			//loop through randomized array 
-			for(Integer currentQuestion:index){
-				//Display letter at current element
-				System.out.println(alphabetArray[currentQuestion]);
-				//Get user input
-				Scanner scanner = new Scanner(System.in);
-				String input = scanner.next();
-				//If input matches answer increment point literal
-				if(input.equalsIgnoreCase(ans[currentQuestion])){
-					points++;
-				}
-				//Log wrong answers to display at the end
-				else{
-					incorrectAnswers[currentQuestion]=input;
-				}
-			}
+			points = printQuestions(intNumQuestions, index, alphabetArray,incorrectAnswers, ans,points);
 		}
 		//If user wants a certain number of questions
 		else {
 			//Convert String input into an Integer
-			int intNumQuestions = Integer.parseInt(inputNumQuestions);
-			for(int currentQuestion = 0; currentQuestion < intNumQuestions; currentQuestion++){
-				int phoneticAlphaIndex = index[currentQuestion];
-				System.out.println(alphabetArray[phoneticAlphaIndex]);
-				Scanner scanner = new Scanner(System.in);
-				String input = scanner.next();
-				if(input.equalsIgnoreCase(ans[phoneticAlphaIndex])){
-					points++;
-				}
-				else{
-					incorrectAnswers[phoneticAlphaIndex]=input;
-				}
-			}
+			intNumQuestions = Integer.parseInt(inputNumQuestions);
+			points = printQuestions(intNumQuestions, index, alphabetArray,incorrectAnswers, ans, points);
 		} 
-		//Print out number of points
+		printResults(points, intNumQuestions,incorrectAnswers,ans);
 		System.out.println("\n");
-		System.out.println("You got "+points+"/"+inputNumQuestions + ". These are the ones you got wrong:");
-		//Print out incorrect answers
-		Object[] headers={"Answer", "You Wrote"};
-		System.out.format("%-9s %-10s\n", headers);
-		System.out.println("------------------");
-		for(String wrong:incorrectAnswers){
-			if(wrong != null){
-				int find = Arrays.asList(incorrectAnswers).indexOf(wrong);
-				Object[] row = {ans[find],wrong};				
-				System.out.format("%-10s%-10s\n", row);
-			}	
-		}
 		System.out.println("Do you want to play again? (Y/N): ");
 		Scanner scannerCont = new Scanner(System.in);
 		inputCont = scannerCont.next();
 	}
+	}
+	public static int printQuestions(int intNumQuestions, Integer[] index, String[] alphabetArray, String[] incorrectAnswers, String[] ans, int points){
+		for(int currentQuestion = 0; currentQuestion < intNumQuestions; currentQuestion++){
+			int phoneticAlphaIndex = index[currentQuestion];
+			System.out.println(alphabetArray[phoneticAlphaIndex]);
+			Scanner scanner = new Scanner(System.in);
+			String input = scanner.next();
+			if(input.equalsIgnoreCase(ans[phoneticAlphaIndex])){
+				points++;
+			}
+			else{
+				incorrectAnswers[phoneticAlphaIndex]=input;
+			}
+		}
+		return(points);
+	}
+
+	public static void printResults(int points, Integer intNumQuestions, String[] incorrectAnswers, String[] ans){
+		//Print out number of points
+		System.out.println("\n");
+		System.out.println("You got "+points+"/"+intNumQuestions + ".");
+		if(points != intNumQuestions){
+			//Print out incorrect answers
+			System.out.println("These are the ones you got wrong:");
+			Object[] headers={"Answer", "You Wrote"};
+			System.out.format("%-9s %-10s\n", headers);
+			System.out.println("------------------");
+			for(String wrong:incorrectAnswers){
+				if(wrong != null){
+					int find = Arrays.asList(incorrectAnswers).indexOf(wrong);
+					Object[] row = {ans[find],wrong};				
+					System.out.format("%-10s%-10s\n", row);
+				}	
+			}
+		}
 	}
 }
